8889617230c1724873ad141de53f9f90bcdceb7c
sohot8653 <kmbin92@gmail.com>
2024-06-07 01:36:06 +0900

    [actuator test]

--- inner/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ inner/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -29,8 +29,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/user/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("118.34.158.21")
+                .hasIpAddress("210.100.242.159")
                 .and()
                 .addFilter(getAuthentionFilter());
 
