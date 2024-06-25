2ec9ded7793da2f855cfec156961d2a40fd9d396
sohot8653 <kmbin92@gmail.com>
2024-06-26 01:09:59 +0900

    [IP -> SERVICE NAME]

--- inner/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ inner/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -21,7 +22,8 @@ public class UserServiceApplication {
     }
     
     @Bean
-    public RestTemplate getRestTemplate() {
+    @LoadBalanced
+    RestTemplate getRestTemplate() {
     	return new RestTemplate();
     }
 }
